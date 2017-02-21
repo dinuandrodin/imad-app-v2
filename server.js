9af3c87e02368844a1359a97f18f07eb039adcf2
dinuandrodin <dinu6796@gmail.com>
2017-02-21 20:26:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var hometemp = { 
+    title:' Home | Dinesh Kumar',
+    heading:'Home',
+    date:'July 6, 1996',
+    content:
+          `<p>This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!
+          </p>
+          <p>This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!
+          </p>
+          <p>This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!This is content of the article where you will be finding everything!
+          </p>`
+};
+function createTemplate (data) { 
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate=`
+ <html>
+  <head>
+      <title>
+          ${title}
+      </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+       <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+  <body>
+      <div class="container">
+         <div>
+          <a href ="/">${heading}</a>
+         </div>
+         <hr/>
+         <h4>This is the Home page!</h4>
+         <div>
+          ${date}
+         </div>
+         <div>
+          ${content}
+         </div>
+      </div>
+  </body>
+ </html>
+
+`;
+ return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/home', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'home.html')); 
+   res.send(createTemplate(hometemp));
 });
 
 app.get('/about', function (req, res) {
