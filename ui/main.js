7847430bd4ffacc09c215a525cd0a3af5498da93
dinuandrodin <dinu6796@gmail.com>
2017-02-22 21:22:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,11 +6,12 @@ element.innerHTML = 'NEW-VALUE';
 
 //move the image
 var img = document.getElementById('madi');
+var marginLeft =0;
 function moveRight () {
-    marginLeft = marginLeft + 100;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onClick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
    // img.style.marginLeft = '100px';
 };
