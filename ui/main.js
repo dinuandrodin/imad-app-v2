351b73fd95ed0fd60d79124696fa94e2e57ca8ee
dinuandrodin <dinu6796@gmail.com>
2017-02-23 20:59:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 
 //change the text of the main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = 'NEW-VALUE';
+//var element = document.getElementById('main-text');
+//element.innerHTML = 'NEW-VALUE';
 
 //move the image
 var img = document.getElementById('madi');
