0fc55eab9778209a5da48f74afe4e3f85185e5e7
dinuandrodin <dinu6796@gmail.com>
2017-02-25 12:46:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,8 @@
-console.log('Loaded!');
-
-//change the text of the main-text div
-//var element = document.getElementById('main-text');
-//element.innerHTML = 'NEW-VALUE';
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft =0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onClick = function () {
-    var interval = setInterval(moveRight, 50);
-   // img.style.marginLeft = '100px';
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick() = function () {
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
