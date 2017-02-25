d57fe6e6115ee3027ed45261962e9106b4d9fa7b
dinuandrodin <dinu6796@gmail.com>
2017-02-25 13:11:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,28 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function() {
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //make a request to the counter endpoint
+    var request = new XMLHTTPRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readystate === XMLHTTPRequest.DONE) {
+            //take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    //render the variable in the current span
+    //counter = counter + 1;
+   // var span = document.getElementById('count');
+   // span.innerHTML = counter.toString();
+   
+   // make a request
+   request.open('GET', 'http://dinuandrodin.imad.hasura-app.io/counter' , true);
+   request.send(null);
 };
